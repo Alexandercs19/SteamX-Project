fc4def88f832d4d607b0db81182eca370eaa4128
Yuneno <111202711+Yuneno@users.noreply.github.com>
2024-01-22 16:34:31 -0500

    fourth o como se diga commit

--- steamx/src/app/app.component.ts
+++ steamx/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- steamx/src/app/upho-cleaning/upho-cleaning.component.ts
+++ steamx/src/app/upho-cleaning/upho-cleaning.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatSliderModule} from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-upho-cleaning',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatSliderModule, FormsModule],
   templateUrl: './upho-cleaning.component.html',
   styleUrl: './upho-cleaning.component.css'
 })
 export class UphoCleaningComponent {
-
+  opacityValue = 0.5;
 }
